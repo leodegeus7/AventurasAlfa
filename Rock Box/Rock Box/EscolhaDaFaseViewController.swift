be57b6e0d4d172a1579b0e8c8cd75cad268cd2f0
Leonardo Geus <leonardodegeus@gmail.com>
2015-06-22 16:39:47 -0300

    estrelas dos níveis

--- Rock Box/Rock Box/EscolhaDaFaseViewController.swift
+++ Rock Box/Rock Box/EscolhaDaFaseViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class EscolhaDaFaseViewController: UIViewController {
     
-    
+    @IBOutlet weak var primeiraFase: UIButton!
+    @IBOutlet weak var segundaFase: UIButton!
+    @IBOutlet weak var terceiraFase: UIButton!
+    @IBOutlet weak var quartaFase: UIButton!
+    @IBOutlet weak var quintaFase: UIButton!
+    @IBOutlet weak var sextaFase: UIButton!
+    @IBOutlet weak var setimaFase: UIButton!
+    @IBOutlet weak var oitavaFase: UIButton!
+    @IBOutlet weak var nonaFase: UIButton!
     
     @IBAction func irParaFase1(sender: AnyObject) {
         DataManager.instance.faseEscolhida = 1
@@ -54,7 +62,31 @@ class EscolhaDaFaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var json = DataManager.instance.lerArquivoJson()
+        var fases = DataManager.instance.arrayDaFaseAntes(1)
+        var estrelas:Int!
+        
+        
+        for index in 1...json.count{
+                fases = DataManager.instance.arrayDaFaseAntes(index)
+                estrelas = fases["quantasEstrelasPegou"] as! Int
+
+            if estrelas == 0 {
+                mudarEstrelas(UIImage(named: "planetavermelho.png")!, fase: index+1)
+            }
+            else if estrelas == 1 {
+                mudarEstrelas(UIImage(named: "menu_planeta_1estrela.png")!, fase: index+1)
+            }
+            else if estrelas == 2 {
+                mudarEstrelas(UIImage(named: "menu_planeta_2estrela.png")!, fase: index+1)
+            }
+            else if estrelas == 3 {
+                mudarEstrelas(UIImage(named: "menu_planeta_3estrela.png")!, fase: index+1)
+            }
+        
+        
+        }
+       
     }
     
     override func prefersStatusBarHidden() -> Bool {
@@ -64,6 +96,41 @@ class EscolhaDaFaseViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mudarEstrelas (imagem:UIImage,fase:Int) {
+        switch fase {
+        case 1:
+            primeiraFase.setImage(imagem, forState: UIControlState.Normal)
+
+            break
+        case 2:
+            segundaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 3:
+            terceiraFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 4:
+            quartaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 5:
+            quintaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 6:
+            sextaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 7:
+            setimaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 8:
+            oitavaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        case 9:
+            nonaFase.setImage(imagem, forState: UIControlState.Normal)
+            break
+        default:
+            break
+        }
     }
     
 
