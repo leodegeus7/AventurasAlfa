437ee7eefd328fdad473ab3431e8b71dfcbb4111
Leonardo Koppe Malanski <Leonardo@Leonardo-Malanski.local>
2015-06-26 17:36:54 -0300

    tela de créditos (que não vai ser usada D; )

---
+++ Rock Box/CreditosViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CreditosViewController.swift
+//  Rock Box
+//
+//  Created by Leonardo Koppe Malanski on 26/06/15.
+//  Copyright (c) 2015 Leonardo Geus. All rights reserved.
+//
+
+import UIKit
+
+class CreditosViewController: UIViewController {
+
+    @IBOutlet weak var alfa: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        alfa.transform = CGAffineTransformMakeRotation(6.02138592)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
